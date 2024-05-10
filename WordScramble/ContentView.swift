131437c1a34cfce2329047c78d7f9480ad21b3ad
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-10 17:07:32 +0100

    Connecting start.txt and having the app choose random word from it. Force exit if start.txt fails to load.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -33,9 +33,8 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(rootWord)
-            .onSubmit {
-                addNewWord()
-            }
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
     }
     
@@ -53,6 +52,20 @@ struct ContentView: View {
         
         newWord = ""
     }
+    
+    func startGame() {
+        // find the url for starttxt in the bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "/n")
+                // pick a random element and assign it - remember it will be optional
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        fatalError("Could not load start.txt from Bundle")
+    }
+    
 }
 
 #Preview {
