a54fce33dcacd4e9e285fcbb6b5b7c4836fc168e
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-10 15:49:38 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
